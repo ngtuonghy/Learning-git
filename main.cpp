01e9c668abfac5fd8ea26c91b0e0b9e7a4bbfe09
ngtuonghy <ngtuonghy@outlook.com>
2023-06-09 17:59:39 +0700

    add func add template

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+template <class T> T add(T a, T b) { return a + b; }
+
 int main(int argc, char *argv[]) {
-  std::cout << "xin chao the gioi   ";
+  add(18, 17);
   return 0;
 }
