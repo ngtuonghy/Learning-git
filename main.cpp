8d90b693714f17a6e8097f431c7a5ead2fa75854
ngtuonghy <ngtuonghy@outlook.com>
2023-06-12 14:24:41 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ template <class T> T add(T a, T b) { return a + b; }
 
 int main(int argc, char *argv[]) {
   add(18, 17);
-  add(1.3,1.5);
+  add(1.3, 1.5);
+  std::cout << "xin chao the gioi";
   return 0;
 }
