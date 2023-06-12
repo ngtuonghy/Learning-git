c2a82781a5186c0f53c4ac6adbe451158671a4c3
ngtuonghy <ngtuonghy@outlook.com>
2023-06-12 14:26:49 +0700

    fix error

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@ template <class T> T add(T a, T b) { return a + b; }
 
 int main(int argc, char *argv[]) {
   add(18, 17);
-<<<<<<< HEAD
   add(1.3, 1.5);
   std::cout << "xin chao the gioi";
-=======
-  add(1.3,1.5);
-  std::cout<< "hello";
->>>>>>> 99cace88ee86a98e974eabf5ad215ddb60075aef
+  add(1.3, 1.5);
+  std::cout << "hello";
   return 0;
 }
